9923c75e6296e49bde9976649c5eb00186c9c421
tripp <tripp.beckers@telenet.be>
2026-02-28 17:35:14 +0100

    made some default front end code to test the deployment

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <div className="flex-shrink-0 flex items-center">
+          <div className="shrink-0 flex items-center">
             <Link href="/" className=" font-bold text-xl tracking-tight">
               🌿 Green Blanket
             </Link>
