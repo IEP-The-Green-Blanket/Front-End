b7c01180009d5f97204abd1de2540a0d497c0ce0
Kevin Coomans (DevKev) <alt.kevin.coomans@gmail.com>
2026-03-21 02:40:09 +0100

    Add: base page for the report form

---
+++ app/reporting/page.tsx
@@ -0,0 +1,16 @@
+import ReportForm from '@/components/ReportForm';
+
+const LoginPage = async () => {
+    return (
+        <>
+            <main>
+                <span>
+                    <section>
+                        <ReportForm />
+                    </section>
+                </span>
+            </main>
+        </>
+    );
+};
+export default LoginPage;
