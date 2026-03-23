e46d8555304ffafefb551d16ca2216bd9056f275
Kevin Coomans (DevKev) <alt.kevin.coomans@gmail.com>
2026-03-23 17:10:35 +0100

    Add: button to navigate to the /reporting

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link";
+import HeaderButtonsBar from "./HeaderButtonsBar";
 
 export default function Header() {
   return (
     <header className="site-header">
 
       {/* 'Mobile' layout */}
-      <div className="header-inner mobile-header">
-        <nav className="navLeftSide">
-          <Link href="/">Home</Link>
-        </nav>
-        <Link href="/" className="logo-link">
-          <img src="/images/Green_Blanket.png" alt="..." className="logo" />
-        </Link>
-        <nav className="navRightSide">
-          <Link href="/">Login</Link>
-        </nav>
+      <div className="header-inner mobile-header flex flex-col items-center w-full">
+          <nav className="navLeftSide">
+            <Link href="/">Home</Link>
+          </nav>
+          <Link href="/" className="logo-link">
+            <img src="/images/Green_Blanket.png" alt="..." className="logo" />
+          </Link>
+          <nav className="navRightSide">
+            <Link href="/">Login</Link>
+          </nav>
       </div>
+      <HeaderButtonsBar/>
 
       {/* 'Desktop' layout */}
       <div className="header-inner desktop-header">
