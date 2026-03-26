e99c2de6f0387a00f31eea68c1bcd0ea4c66d465
Kevin Coomans (DevKev) <alt.kevin.coomans@gmail.com>
2026-03-26 11:15:49 +0100

    Update: new layout to the header-buttons bar

--- components/HeaderButtonsBar.tsx
+++ components/HeaderButtonsBar.tsx
@@ -4,16 +4,15 @@ export default function HeaderButtonsBar() {
     return (
         <div className="w-full">
             <ul className="flex w-full gap-4">
-                <li className="flex-1 text-center">
+                <li className="flex-1 justify-center">
                     <Link href="/reporting">
                         <img 
-                            src="/images/hdr_reporting_btn.png" 
+                            src="/images/btn_alert_us.png" 
                             alt="button reading alert us surrounded by a green frame with a megaphone in the corner."
                             className="m-2 mt-0 max-h-11 transition-transform duration-150 active:scale-90" />
                     </Link>
                 </li>
             </ul>
         </div>
-
     );
 }
